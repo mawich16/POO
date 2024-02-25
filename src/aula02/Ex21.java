2b0c542e82b1fa5316fcbef8676373ed4e17616c
mawich <martacondeco@ua.pt>
2024-02-25 15:13:30 +0000

    update

--- src/aula02/Ex21.java
+++ src/aula02/Ex21.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class Ex21 {
     public static void main(String[] args) {
+        double conversionRate = 1.609;
         Scanner sc = new Scanner(System.in);
+
         System.out.print("quantos quilometros? ");
-        double milhas = sc.nextDouble() / 1.609;
-        System.out.println("esse valor é equivalente a " + milhas + " milhas");
+        double kms = sc.nextDouble();
         sc.close();
+
+        double miles = kms / conversionRate;
+        System.out.println("esse valor é equivalente a " + miles + " milhas");
     }
 }
